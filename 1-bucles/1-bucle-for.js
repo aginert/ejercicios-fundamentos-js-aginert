2cc01a831918442626d3755790636d8574c157a2
aginert <ana.ginert@gmail.com>
2018-10-27 13:40:21 +0200

    Primer ejercicio resuelto

--- 1-bucles/1-bucle-for.js
+++ 1-bucles/1-bucle-for.js
@@ -2,6 +2,6 @@
 // haz un bucle que muestre por consola los números 
 //del 4 al 9 inclusive
 // Utilizad el bucle for
-for(var i = 4; i < 9; i++){
+for(var i = 4; i < 10; i++){
     console.log(i);
 }
