767f58de74936cd2b713de4737c81e3ccf5a3a3b
aginert <ana.ginert@gmail.com>
2018-10-27 18:49:36 +0200

    ejercicio 8 resuelto con  operador ternario

--- 1-bucles/8-fizzbuzz.js
+++ 1-bucles/8-fizzbuzz.js
@@ -5,22 +5,32 @@
 // Además los múltiplos de 3 y 5 ha de imprimir GEEKSHUBS
 
 
-function geeksHubs() {
-    for ( var i = 1; i <= 100; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            console.log("GEEKSHUBS");
+// function geeksHubs() {
+//     for ( var i = 1; i <= 100; i++) {
+//         if (i % 3 == 0 && i % 5 == 0) {
+//             console.log("GEEKSHUBS");
 
-        }else if (i % 5 == 0) {
-            console.log("HUBS");
+//         }else if (i % 5 == 0) {
+//             console.log("HUBS");
 
-        } else if (i % 3 == 0) {
-            console.log("GEEKS")
+//         } else if (i % 3 == 0) {
+//             console.log("GEEKS")
 
-        } else {
-            console.log(i);
-        }
-    }
-}
+//         } else {
+//             console.log(i);
+//         }
+//     }
+// }
 
-geeksHubs();
+// geeksHubs();
+
+
+// function geeksHubsTer() {
+    for(var i = 1; i <= 100; i++){
+        var e = i%3 & i%5 == 0;
+        var b = i%5 == 0;
+        var c = i%3 == 0;   
+        console.log( e? "GEEKS HUBS":  b? "HUBS": c? "GEEKS": i);
+    }  
+// } 
 
