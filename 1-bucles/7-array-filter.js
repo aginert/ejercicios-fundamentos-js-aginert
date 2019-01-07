3dd9c5d4ef941fa7167b472f2b90e4dd439c148c
aginert <ana.ginert@gmail.com>
2019-01-07 14:46:59 +0100

    Resuelto

--- 1-bucles/7-array-filter.js
+++ 1-bucles/7-array-filter.js
@@ -1,5 +1,5 @@
-// ej 7
-// crea un array con la gente que su nombre
+// Ejemplo 7
+// Crea un array con la gente que su nombre
 // empieza por J y sean menores de 40 años
 // utilizando el método filter de los arrays
 // y muéstralo por consola.
@@ -38,15 +38,12 @@ var gente = [{
     },
 ]
 
-// var arrayDosCondi = [];
-
 arrayDosCondi = gente.filter(personas => personas.nombre [0] === "j"; personas.edad<40)
 console.log(arrayDosCondi);
 
 // var dosCondiciones;
+
 // var dosCondiciones = gente.filter(function(persona){
 //     return persona.nombre[0]=="J"; persona.edad <40
 // });
 // console.log(dosCondiciones); 
-
-
