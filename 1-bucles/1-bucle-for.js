6f75e292f83077a346eaea0fc4a5fc0c3978fcb9
aginert <ana.ginert@gmail.com>
2019-01-07 13:33:00 +0100

    Resuelto

--- 1-bucles/1-bucle-for.js
+++ 1-bucles/1-bucle-for.js
@@ -1,7 +1,8 @@
-// ej 1
-// haz un bucle que muestre por consola los números 
-//del 4 al 9 inclusive
+// Ej 1
+// Haz un bucle que muestre por consola los números 
+// del 4 al 9 inclusive
 // Utilizad el bucle for
+
 for(var i = 4; i < 10; i++){
     console.log(i);
 }
