8dc0beaf1002217f64347d6b0508e6229e138079
aginert <ana.ginert@gmail.com>
2019-01-07 14:49:28 +0100

    Resuelto

--- 1-bucles/9-array-bidimensional.js
+++ 1-bucles/9-array-bidimensional.js
@@ -7,19 +7,28 @@
 //   ['20','21','22']
 // ];
 
+let matriz = [
+   [],
+   [],
+   []
+]
+
+// ej 9
+// Rellena la matriz como la siguiente utilizando bucles for anidados
+// let matriz = [
+//   ['00','01','02'],
+//   ['10','11','12'],
+//   ['20','21','22']
+// ];
 
 
-var matriz = [
-    [],
-    [],
-    []
-];
- for(var i = 0; i < 3; i++){
-    for(var j = 0; j < 3; j++){
+
+for(var i = 0; i < 3; i++){
+   for(var j = 0; j < 3; j++){
        matriz [i][j]= ""+i+j;
-    
-    }
- }
- console.log(matriz);
+
+   }
+}
+console.log(matriz);
 
  
