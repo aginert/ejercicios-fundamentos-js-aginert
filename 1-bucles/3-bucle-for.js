9235806339f329a0694d983eb579b09f52a38444
aginert <ana.ginert@gmail.com>
2019-01-07 13:36:38 +0100

    Resuelto + sencillo

--- 1-bucles/3-bucle-for.js
+++ 1-bucles/3-bucle-for.js
@@ -1,16 +1,15 @@
-// ej 3
-// muestra por consola la tabla del 7;
+// Ejercicio 3
+// Muestra por consola la tabla del 7;
 // Utilizad el bucle for
-//meteriamos un bucle for dentro de otro para que nos imprimiese las tablas de 1 al 10
+// Meteriamos un bucle for dentro de otro para que nos imprimiese las tablas de 1 al 10
+
 //for(var i = 1; i <=10; i++) {
 //   for(var j = 1; j <=10; j++) {
 //     console.log(i+" * "+j+" = "+ i*j );
 //    }
- // }
+// }
+
 var j = 7;
 
 for (var i = 1; i <=10; i++ ) {
     console.log(j+" * "+i+" = "+ j*i);
-}
-
-  
